02a14371b79f5bbdc5476c0321812803b93298dc
pablopaezsheridan <pablopaezps97@gmail.com>
2023-11-13 21:28:28 -0300

    Miscelanious: remove comments in authenticate controller

--- ConsultaAlumnoEnClaseTarde/Controllers/AuthenticateController.cs
+++ ConsultaAlumnoEnClaseTarde/Controllers/AuthenticateController.cs
@@ -25,7 +25,6 @@ namespace ConsultaAlumnos2TUP4.Controllers
         [HttpPost]
         public IActionResult Authenticate([FromBody] CredentialsDto credentialsDto)
         {
-            //valido usuario
             BaseResponse validarUsuarioResult = _userService.ValidarUsuario(credentialsDto.Email, credentialsDto.Password);
             if (validarUsuarioResult.Message == "wrong email")
             {
@@ -37,21 +36,18 @@ namespace ConsultaAlumnos2TUP4.Controllers
             }
             if (validarUsuarioResult.Result)
             {
-                //generacion del token
                 User user = _userService.GetUserByEmail(credentialsDto.Email);
-                //Paso 2: Crear el token
-                var securityPassword = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["Authentication:SecretForKey"])); //Traemos la SecretKey del Json. agregar antes: using Microsoft.IdentityModel.Tokens;
+                var securityPassword = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["Authentication:SecretForKey"]));
 
                 var signature = new SigningCredentials(securityPassword, SecurityAlgorithms.HmacSha256);
 
-                //Los claims son datos en clave->valor que nos permite guardar data del usuario.
                 var claimsForToken = new List<Claim>();
-                claimsForToken.Add(new Claim("sub", user.Id.ToString())); //"sub" es una key estándar que significa unique user identifier, es decir, si mandamos el id del usuario por convención lo hacemos con la key "sub".
+                claimsForToken.Add(new Claim("sub", user.Id.ToString())); 
                 claimsForToken.Add(new Claim("email", user.Email));
                 claimsForToken.Add(new Claim("username", user.UserName));
-                claimsForToken.Add(new Claim("role", user.UserType)); //Debería venir del usuario
+                claimsForToken.Add(new Claim("role", user.UserType));
 
-                var jwtSecurityToken = new JwtSecurityToken( //agregar using System.IdentityModel.Tokens.Jwt; Acá es donde se crea el token con toda la data que le pasamos antes.
+                var jwtSecurityToken = new JwtSecurityToken(
                     _config["Authentication:Issuer"],
                     _config["Authentication:Audience"],
                     claimsForToken,
